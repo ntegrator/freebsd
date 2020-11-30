789d605cdb76538994b8b44371f80b0ca8f852b7
ntegrator <74719847+ntegrator@users.noreply.github.com>
2020-11-30 14:32:02 +0300

    config check / optimization

--- sync_freebsd_repo.py
+++ sync_freebsd_repo.py
@@ -1,3 +1,6 @@
+from configparser import ConfigParser, NoOptionError
+
+
 def rm_files(path):
     from os import remove
     import glob
@@ -34,7 +37,7 @@ def mkdir(path):
         exit()
 
 
-def down_file(url, path):
+def download_file(url, path):
     from os.path import isfile, dirname, exists
     from os import remove
     from urllib import request
@@ -56,18 +59,14 @@ def down_file(url, path):
         exit()
 
 
-def urlretrieve_and_check(url, path, shasum, tries=0):
-    from sys import exit
-    if tries < 0 or type(tries) is not int:
-        print("Incorrect tries! Exiting")
-        exit()
+def download_and_check(url, path, shasum, tries):
     count = 0
     if tries == 0:
         while sha256_file(path) != shasum:
-            down_file(url, path)
+            download_file(url, path)
     else:
         while count < tries and sha256_file(path) != shasum:
-            down_file(url, path)
+            download_file(url, path)
             count += 1
 
 
@@ -110,7 +109,50 @@ def percent(count, total):
     return str(int(count / (total / 100))) + '%'
 
 
-def sync_repo(name, path, arch, mirror_url, release, tries):
+def create_config(path):
+    config.add_section("example_repo")
+    config.set("example_repo", "path", "/repo")
+    config.set("example_repo", "mirror_url", "https://pkg.freebsd.org")
+    config.set("example_repo", "arch", "FreeBSD:12:amd64")
+    config.set("example_repo", "release", "latest")
+    config.set("example_repo", "fast", "yes")
+    config.set("example_repo", "tries", "0")
+
+    with open(path, "w") as config_file:
+        config.write(config_file)
+
+
+def except_var_error(section, var, default_value):
+    print("Warning!\n"
+          "Incorrect or empty variable in section " + section + ": " + var + "\n"
+          "Use the default value: " + var + " = " + default_value)
+    config.set(section, var, default_value)
+
+
+def check_config(path):
+    from os.path import isfile
+    if not isfile(path) or not config.sections():
+        print("Warning!\n"
+              "File settings.ini is missing or empty!\n"
+              "Creating a new file settings.ini, using the default settings\n"
+              "Please change it!")
+        create_config(path)
+        exit()
+
+    for i in config.sections():
+        try:
+            if config.get(i, "fast") != "yes" and config.get(i, "fast") != "no":
+                except_var_error(i, "fast", "yes")
+        except (NoOptionError, ValueError):
+            except_var_error(i, "fast", "yes")
+        try:
+            if not 0 <= int(config.get(i, "tries")):
+                except_var_error(i, "tries", "0")
+        except (NoOptionError, ValueError):
+            except_var_error(i, "tries", "0")
+
+
+def sync_repo(name, path, arch, mirror_url, release, fast, tries):
     from os.path import basename, exists
     from os import listdir, remove
     from json import loads
@@ -119,13 +161,12 @@ def sync_repo(name, path, arch, mirror_url, release, tries):
 
     print("\nStarting syncing repository", name + " (" + arch + "):")
 
-    print("Cleaning the", path, "directory...")
-    rm_files(local_path + release + "/*.txz")
-    rm_files(local_path + release + "/*.conf")
-    rm_files(local_path + release + "/Latest/pkg*")
+    if exists(local_path + "temp"):
+        print("Cleaning the temp directory...")
+        rm_dir(local_path + "temp")
 
     print("Downloading packagesite.txz...")
-    down_file(mirror_path + "packagesite.txz", local_path + "temp/packagesite.txz")
+    download_file(mirror_path + "packagesite.txz", local_path + "temp/packagesite.txz")
 
     print("Unpacking packagesite.txz...")
     untar(local_path + "temp/packagesite.txz", local_path + "temp/")
@@ -139,8 +180,14 @@ def sync_repo(name, path, arch, mirror_url, release, tries):
             print_flush("Processing: " + basename(data["path"]))
     print_flush('')
 
+    print("Removing old metafiles in", local_path + "...")
+    files = {"Latest/pkg-devel.txz", "Latest/pkg.txz", "Latest/pkg.txz.sig",
+             "meta.conf", "meta.txz", "packagesite.txz"}
+    for file in files:
+        rm_files(local_path + file)
+
     if exists(local_path + "All/"):
-        print("Removing old packages...")
+        print("Removing old packages in", local_path + "All/...")
         for i in listdir(local_path + "All/"):
             if i not in pkgs:
                 print_flush("Removing the " + i)
@@ -148,19 +195,27 @@ def sync_repo(name, path, arch, mirror_url, release, tries):
     print_flush('')
 
     print("Downloading", arch + "...")
-    files = {"Latest/pkg-devel.txz", "Latest/pkg.txz", "Latest/pkg.txz.sig",
-             "meta.conf", "meta.txz", "packagesite.txz"}
     for file in files:
-        print_flush("Downloading: " + basename(file))
-        down_file(mirror_path + file, local_path + file)
+        print_flush("Downloading metafile: " + basename(file))
+        download_file(mirror_path + file, local_path + file)
 
     count = 0
     all_pkgs = len(pkgs)
-    for i in pkgs.keys():
-        file_path = local_path + "All/" + i
-        print_flush(percent(count, all_pkgs) + " Download / check shasum: " + i)
-        urlretrieve_and_check(mirror_path + "All/" + i, file_path, pkgs.get(i), int(tries))
-        count += 1
+    if fast == "no":
+        for i in pkgs.keys():
+            file_path = local_path + "All/" + i
+            print_flush(percent(count, all_pkgs) + " Download / check shasum: " + i)
+            download_and_check(mirror_path + "All/" + i, file_path, pkgs.get(i), int(tries))
+            count += 1
+    elif fast == "yes":
+        if exists(local_path + "All/"):
+            for i in listdir(local_path + "All/"):
+                del pkgs[i]
+                all_pkgs -= 1
+        for i in pkgs.keys():
+            print_flush(percent(count, all_pkgs) + " Download: " + i)
+            download_file(mirror_path + "All/" + i, local_path + "All/" + i)
+            count += 1
     print_flush('')
 
     print("Cleaning the temp directory...")
@@ -170,14 +225,15 @@ def sync_repo(name, path, arch, mirror_url, release, tries):
 
 
 def main():
-    from configparser import ConfigParser
-    config = ConfigParser()
-    config.read("settings.ini")
+    check_config("settings.ini")
 
     for i in config.sections():
-        sync_repo(i, config[i]["path"], config[i]["arch"], config[i]["mirror_url"],
-                  config[i]["release"], config[i]["tries"])
+        sync_repo(i, config.get(i, 'path'), config.get(i, 'arch'),
+                  config.get(i, 'mirror_url'), config.get(i, 'release'),
+                  config.get(i, 'fast'), config.get(i, 'tries'))
 
 
 if __name__ == '__main__':
+    config = ConfigParser()
+    config.read("settings.ini")
     main()
